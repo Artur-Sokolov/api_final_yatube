313cb4948a8219109d5859df725fffe1e0fe9e1e
Artur Sokolov <roshan9416@gmail.com>
2025-04-15 22:04:53 -0400

    1.4

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -26,6 +26,15 @@ class Comment(models.Model):
         'Дата добавления', auto_now_add=True, db_index=True)
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
