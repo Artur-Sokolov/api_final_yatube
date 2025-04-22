08c6dc3307a52a3f79655846b943b77a4848af69
Artur Sokolov <roshan9416@gmail.com>
2025-04-21 20:54:48 -0400

    4.1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -25,23 +25,6 @@ class FollowViewSet(mixins.ListModelMixin,
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED,
-            headers=headers
-        )
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
